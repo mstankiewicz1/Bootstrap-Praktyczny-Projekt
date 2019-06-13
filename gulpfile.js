7b550088b1b8a45a885a3701c8bd025dbcdf749b
Marek Stankiewicz <marekstankiewicz6@wp.pl>
2019-06-14 00:31:36 +0200

    Postępy 14.06.19r godz 00:31

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass  = require('gulp-sass');
+
+var cssDest = 'style';
+var cssInputFile = 'source/style.scss';
+
+gulp.task('buildcss', function () {
+   return gulp.src(cssInputFile)
+    .pipe(sass({
+        outputStyle: 'compressed'
+    }))
+   .pipe(gulp.dest(cssDest));
+});
+
