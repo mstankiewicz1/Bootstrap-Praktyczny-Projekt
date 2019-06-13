8223feb6c80b47fc82e745ff441cd3b1d22247be
Marek Stankiewicz <marekstankiewicz6@wp.pl>
2019-06-14 01:24:04 +0200

    Postepy 14.06.19r godz 01:24

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var sass  = require('gulp-sass');
 
 var cssDest = 'style';
 var cssInputFile = 'source/style.scss';
+var watchedFiles = 'source/**/*.scss';
 
 gulp.task('buildcss', function () {
    return gulp.src(cssInputFile)
@@ -10,5 +11,9 @@ gulp.task('buildcss', function () {
         outputStyle: 'compressed'
     }))
    .pipe(gulp.dest(cssDest));
+});
+
+gulp.task('watch', function () {
+   gulp.watch(watchedFiles, ['buildcss']);
 });
 
